f21dfd6aad4f8d7968c7eefb07284ca97d6af74a
SimonDanisch <sdanisch@protonmail.com>
2020-12-21 15:01:38 +0100

    initial commit

--- src/MemoryHunter.jl
+++ src/MemoryHunter.jl
@@ -1,5 +1,36 @@
 module MemoryHunter
 
-# Write your package code here.
+function print_sizes(threshold=5#=mb=#)
+    @show Base.format_bytes(Sys.free_memory())
+    biggest_vars = Pair{String, Float64}[]
+    visited = IdDict()
+    # just make sure the numbers just look different because
+    # of gc running inconistantly
+    GC.gc(true)
+    for m in values(Base.loaded_modules)
+        if !haskey(visited, m)
+            visited[m] = true
+            for vs in names(m, all = true)
+                if isdefined(m, vs)
+                    v = getfield(m, vs)
+                    if !haskey(visited, v)
+                        visited[v] = true
+                        x = Base.summarysize(v)
+                        if x > threshold * 10^6
+                            push!(biggest_vars, string(m, ".", vs) => x)
+                        end
+                    end
+                end
+            end
+        end
+    end
+    sort!(biggest_vars, by=last, rev=true)
+    for (var, s) in biggest_vars
+        println(var, ": ", Base.format_bytes(s))
+    end
+end
+print_sizes()
+
+
 
 end
